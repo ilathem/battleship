f7afefeee9b8f22686c2716e563b4b50fd01abf0
ilathem <isaiahlathem@gmail.com>
2022-12-26 21:10:55 -0600

    first commit

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0YA-4E1C0HaKu7zAR3apREMp8xztRNWA",
+  authDomain: "battleship-7f327.firebaseapp.com",
+  projectId: "battleship-7f327",
+  storageBucket: "battleship-7f327.appspot.com",
+  messagingSenderId: "704481721334",
+  appId: "1:704481721334:web:805943bf8576e7ffbe65d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Battleship",
+        short_name: "Battleship",
+        description: "Battleship PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "battleship.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          }
+        ]
+      } 
+    })
+  ],
+})
